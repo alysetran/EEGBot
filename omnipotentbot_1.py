c5d2a096bb3c26ae388ca3ff9132f87a53f844e7
Alyse Tran <atran23@jhu.edu>
2020-07-09 12:05:59 -0700

    new Strategies_black implemented

--- omnipotentbot_1.py
+++ omnipotentbot_1.py
@@ -2,8 +2,7 @@ import random
 from reconchess import *
 from operator import itemgetter
 
-# move sequences from white's perspective, flipped at runtime if playing as black
-Strategies = [
+Strategies_white = [
     # Opening move, knight to protect weak spot in case of Scholar's Mate
     [chess.Move(chess.G1, chess.H3)],
     # Queen side knight attack method #1
@@ -24,18 +23,30 @@ Strategies = [
     [chess.Move(chess.D2, chess.D3), chess.Move(chess.C1, chess.G5)]
 ]
 
+Strategies_black = [
+    # Opening move, knight to protect weak spot in case of Scholar's Mate
+    [chess.Move(chess.G1, chess.H3)],
+    # Queen side knight attack method #1
+    [chess.Move(chess.B8, chess.C6), chess.Move(chess.C6, chess.B4), chess.Move(chess.B4, chess.D3),
+     chess.Move(chess.D3, chess.E1)],
+    # Queen side knight attack method #2
+    [chess.Move(chess.B8, chess.C6), chess.Move(chess.C6, chess.E5), chess.Move(chess.E5, chess.F3),
+     chess.Move(chess.F3, chess.E1)],
+    # Scholar's Mate
+    [chess.Move(chess.E7, chess.E5), chess.Move(chess.F8, chess.C5), chess.Move(chess.D8, chess.H4), chess.Move(
+        chess.C5, chess.F2), chess.Move(chess.F2, chess.E1), chess.Move(chess.H4, chess.E1)],
+    # Castling
+    [chess.Move(chess.E8, chess.G8)],
+    # King side knight attack
+    [chess.Move(chess.H6, chess.F5), chess.Move(chess.D8, chess.H4), chess.Move(
+        chess.C5, chess.F2), chess.Move(chess.F2, chess.E1), chess.Move(chess.H4, chess.E1)],
+    # Bring out the other bishop
+    [chess.Move(chess.D7, chess.D6), chess.Move(chess.C8, chess.G4)]]
 
-def flipped_move(move):
-    def flipped(square):
-        return chess.square(chess.square_file(square), 7 - chess.square_rank(square))
 
-    return chess.Move(from_square=flipped(move.from_square), to_square=flipped(move.to_square),
-                    promotion=move.promotion, drop=move.drop)
-
-
-class OmnipotentBot(Player):
+class omnipotentbot_1(Player):
     def __init__(self):
-        self.move_sequence = list(map(itemgetter(0), Strategies))
+        self.move_sequence = list(map(itemgetter(0), Strategies_white))
         self.turn = None
         self.board = None
         self.color = None
@@ -43,7 +54,7 @@ class OmnipotentBot(Player):
 
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
         if color == chess.BLACK:
-            self.move_sequence = list(map(flipped_move, self.move_sequence))
+            self.move_sequence = list(Strategies_black)
 
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
         pass
@@ -65,6 +76,7 @@ class OmnipotentBot(Player):
     def handle_move_result(self, requested_move: chess.Move, taken_move: chess.Move,
                            captured_opponent_piece: bool, capture_square: Optional[Square]):
         print(self.move_sequence)
+
     pass
 
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
